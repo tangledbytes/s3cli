14ce2d6ca1b3d6b3c200ea7bf2514de48af7dcda
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-10 16:55:16 +0530

    add a string type restricted flag
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ cli/pkg/restrictedflag/restrictedflag.go
@@ -0,0 +1,44 @@
+package restrictedflag
+
+import (
+	"fmt"
+)
+
+type Flag struct {
+	allowed []string
+	value   string
+}
+
+func New(value string, allowed ...string) *Flag {
+	return &Flag{
+		allowed: allowed,
+		value:   value,
+	}
+}
+
+func (f *Flag) Set(value string) error {
+	for _, v := range f.allowed {
+		if v == value {
+			f.value = value
+			return nil
+		}
+	}
+
+	return fmt.Errorf("value %v is not allowed", value)
+}
+
+func (f *Flag) Get() string {
+	return f.value
+}
+
+func (f *Flag) String() string {
+	return f.value
+}
+
+func (f *Flag) Allowed() []string {
+	return f.allowed
+}
+
+func (f *Flag) Type() string {
+	return "string"
+}
