13e51ac1d7cf8e1706ccb6f9994075e805002db4
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2022-09-10 16:57:11 +0530

    main
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- cli/main.go
+++ cli/main.go
@@ -1,54 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-
-	"github.com/utkarsh-pro/s3cli/cli/pkg/aws"
-)
-
-type A struct{}
-
-func setupFlag() map[string]interface{} {
-	flags := make(map[string]interface{})
-
-	flags["endpoint"] = flag.String("endpoint", "", "S3 endpoint")
-	flags["region"] = flag.String("region", "", "S3 region")
-	flags["access-key"] = flag.String("access-key", "", "S3 access key")
-	flags["secret-key"] = flag.String("secret-key", "", "S3 secret key")
-	flags["anon"] = flag.Bool("anon", false, "S3 anonymous")
-	flags["skip-ssl"] = flag.Bool("skip-ssl", false, "S3 skip ssl")
-	flags["api"] = flag.String("api", "", "S3 api name")
-	flags["params"] = flag.String("params", "", "S3 api params")
-	flags["file-params"] = flag.String("file-params", "", "S3 api file params - gets merged with params after file resolution")
-
-	flag.Parse()
-
-	return flags
-}
-
-func ensureRequired(flags map[string]interface{}, required []string) error {
-	for _, r := range required {
-		if _, ok := flags[r]; !ok {
-			return fmt.Errorf("%s flag is required", r)
-		}
-	}
-
-	return nil
-}
+import "github.com/utkarsh-pro/s3cli/cli/cmd"
 
 func main() {
-	flags := setupFlag()
-
-	if err := ensureRequired(flags, []string{"api", "endpoint"}); err != nil {
-		fmt.Println(err)
-		flag.PrintDefaults()
-		return
-	}
-
-	aws.
-		New(aws.AWSConfig{
-			Region: flags["region"].(string),
-		}).
-		RunAny("ListBuckets", map[string]interface{}{}, map[string]string{})
+	cmd.Execute()
 }
